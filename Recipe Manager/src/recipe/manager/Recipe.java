989713084b82d541c28dc92758a2e2d713db2426
Cara Landicho <caralandicho@yahoo.com>
2013-02-15 21:14:20 -0800

    Recipe

---
+++ Recipe Manager/src/recipe/manager/Recipe.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recipe.manager;
+
+/**
+ *
+ * @author caralandicho
+ */
+public class Recipe {
+    /**
+     *
+     */
+    public void createRecipe() {
+        
+    }
+    
+    public void editRecipe() {
+        
+    }
+    
+    public void deleteRecipe() {
+        
+    }
+    
+    public void addIngredient() {
+        
+    }
+    
+    public void deleteIngredient() {
+        
+    }
+    
+    public void addComment() {
+        
+    }
+    
+    public void deleteComment() {
+        
+    }
+    
+    public void addMainIngredient() {
+        
+    }
+    
+    public void deleteMainIngredient() {
+        
+    }
+    
+    public void shareRecipe() {
+        
+    }
+}
